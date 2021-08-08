5a26abf513cd96aee3fd7acb8f3c38e91a0dff87
Hidaka Rintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-08-08 23:52:15 +0900

    [fin] 4-20

--- src/index.js
+++ src/index.js
@@ -48,14 +48,37 @@
 /**
  * テンプレート文字列
  */
-const name = "Rin";
-const age = 22;
-// 「私の名前はRinです。年齢は22歳です。」
+// const name = "Rin";
+// const age = 22;
+// // 「私の名前はRinです。年齢は22歳です。」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
