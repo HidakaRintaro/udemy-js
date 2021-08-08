fadb6338a20b05a07a10bb6dd8307de9ac4bb0df
Hidaka Rintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-08-08 23:43:20 +0900

    [fin] 4-18

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**
+ * const, let 等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var 変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var 変数を再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // let は上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // let は再宣言不可能
+// let val2 = "let変数再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const 変数は上書き不可
+// val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const で定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "りん",
+//   age: 22,
+// };
+// val4.name = "rin";
+// val4.address = "Osaka";
+// console.log(val4);
+
+// const で定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
